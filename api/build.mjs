e7df957cc459f21d814243bb5d1390b7267c2366
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-26 15:22:45 -0400

    Use esbuild to build api

---
+++ api/build.mjs
@@ -0,0 +1,13 @@
+import { build } from "esbuild";
+import { nodeExternalsPlugin } from "esbuild-node-externals";
+
+build({
+  bundle: true,
+  entryPoints: ["src/index.ts"],
+  external: ["@graphql-ts/lib", "cors", "express", "graphql-http"],
+  format: "esm",
+  outfile: "./dist/index.js",
+  platform: "node",
+  plugins: [nodeExternalsPlugin()],
+  sourcemap: true,
+}).catch(() => process.exit(1));
