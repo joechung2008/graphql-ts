f47214fe0b288d5b8a6cacb64dad595314683487
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-26 15:51:18 -0400

    Remove unused default in executeQuery

--- app/src/lib/executeQuery.ts
+++ app/src/lib/executeQuery.ts
@@ -1,15 +1,4 @@
-const defaultQuery = `
-query GetDiagnostics($environment: String! = "https://hosting.portal.azure.net/api/diagnostics") {
-  diagnostics(environment: $environment) {
-    armExtensions {
-      RpServerVersion
-      lastChangedTime
-    }
-  }
-}
-`;
-
-export async function executeQuery(query = defaultQuery) {
+export async function executeQuery(query: string) {
   const response = await fetch("http://localhost:3000/graphql", {
     body: JSON.stringify({
       query,
